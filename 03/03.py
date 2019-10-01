33ac61be780d76246d77b89145f89be2dc102d3c
Yuchen Xia <55283221+ycxia012@users.noreply.github.com>
2019-10-01 19:03:32 +0800

    03

--- 03/03.py
+++ 03/03.py
@@ -1,29 +1,32 @@
+# define all 3 functions 
+def func1():
+    print('Hello, World!')
+
+
+def func2():
+    name = input('Enter your username:')
+    print('Hello,', name, '!')
+
+
+def func3():
+    celsius = float(input('Enter the celsius you want to convert:'))
+    fahrenheit = celsius * 1.8 + 32
+    print(fahrenheit)
+
+    # ------------------------------------------------------------------------------- #
+
+
 while True:
-    n = input('Which function you would like to use? 1,2 or 3? If no, pls enter no for exit!')
-
-#-------------------------------------------------------------------------------#
-    def func1():
-        print('Hello, World!')
+    n = input('Which function you would like to use? 1,2 or 3? If no, enter something else to exit!')
 
 
-    def func2():
-        name = input('Enter your username:')
-        print('Hello,', name, '!')
-
-
-    def func3():
-        celsius = float(input('Enter the celsius you want to convert:'))
-        fahrenheit = celsius * 1.8 + 32
-        print(fahrenheit)
-
-#-------------------------------------------------------------------------------#
     def users(n):
         if n == '1':
             return func1()
 
         elif n == '2':
             return func2()
-        
+
         elif n == '3':
             return func3()
 
@@ -32,5 +35,5 @@ while True:
 
 
     users(n)
-
+# end of function
 
