da71dd8e2216d2c28acd1fe0731bc711bb2f4196
Yuchen Xia <55283221+ycxia012@users.noreply.github.com>
2019-10-15 16:13:51 +0800

    05

--- 05/H1.py
+++ 05/H1.py
@@ -1,5 +1,5 @@
 import json
-
+#function for open the file
 def op_f(filename):
     fh=open(filename)
     data=json.load(fh)
@@ -7,31 +7,32 @@ def op_f(filename):
     return data
     
 
-
+#function for serialize
 def serialize(l):
     s=json.dumps(l)
     return s
 
-        
+ #input files       
 filename=input('H1-1.json,H1-2.json,H1-3.json,H1-4.json or H1-5.json?')
 
+#Load the json file
 d1=op_f(filename)
 print(d1)
 print(type(d1))
 print('Success!')
     
-
+#Convert the data into string
 s=serialize(d1)
 print(type(s)) 
 
-
+#Write the sting to the file
 filename1=input('what will be the name of the new file?')
 with open(filename1,'w') as f_w:
     json.dump(s,f_w)
     f_w.close()
     print('Success!')
 
-
+#Read the string
 with open(filename1) as f_r:
        d2=json.load(f_r)
        f_r.close()
@@ -39,7 +40,58 @@ print(d2)
 print(type(d2))
 print('Success!')
 
+#To deserialization function
 d2=json.loads(d2)
 print(d2)
 print(type(d2))
 
+#function for comparison
+def my_compare(d1,d2):
+    if type(d1) == type(d2):
+        if type(d1) == type([]):
+            print(diff_lst(d1,d2))
+        if type(d1) == type({}):
+            print(diff_dict(d1,d2))
+    else:
+        return False
+
+#function for compare the list
+def diff_lst(d1,d2):
+    if len(d1) == 0:
+        return True
+    v=d1[0]
+    for v1 in d2:
+        if type(v) != type(v1):
+            return False
+
+        if type(v) == list:
+            if not diff_lst(v,v1):
+                return False
+
+        if type(v) == dict:
+            if not diff_dict(v,v1):
+                return False
+
+    return True
+
+#function for comapre the dict
+def diff_dict(d1,d2):
+        for k,v in d1.items():
+            if k not in d2.keys():
+                return False
+            v1 =d2[k]
+
+        if type(v) != type(v1):
+            return False
+
+        elif type(v) == dict:    
+            if not diff_dict(v,v1):
+                return False
+
+        elif type(v) == list:
+            if not diff_lst(v,v1):
+                return False
+
+        return True
+
+my_compare(d1,d2)
